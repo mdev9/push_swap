ba0f4698c4d594cca6d5cbf25cb345ce249fb7b6
mdev9 <devriesmarijn05@gmail.com>
2024-01-21 21:01:01 +0100

    push working

--- push.c
+++ push.c
@@ -6,56 +6,42 @@
 /*   By: marde-vr <marde-vr@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 21:21:58 by marde-vr          #+#    #+#             */
-/*   Updated: 2024/01/21 19:30:28 by marde-vr         ###   ########.fr       */
+/*   Updated: 2024/01/21 20:56:39 by marde-vr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	pa(t_stack **stack_a, t_stack **stack_b)
-{
-	t_stack *first_a;
-	t_stack *first_b;
-	t_stack	*tmp;
-
-	if (stack_size(stack_b) == 0)
-		return ;
-	first_a = *stack_a;
-	while (first_a->next != *stack_a)
-		first_a = first_a->next;
-	tmp = *stack_b;
-	while (tmp->next->next != *stack_b)
-		tmp = tmp->next;
-	first_b = tmp->next;
-	tmp->next = *stack_b;
-	first_a = first_b;
-	first_a->next = *stack_a;
-}
+void	pa(t_stack **stack_a, t_stack **stack_b);
 
 void	pb(t_stack **stack_a, t_stack **stack_b)
 {
 	t_stack *first_a;
-	t_stack	*tmp_a;
-	t_stack	*tmp_b;
+	t_stack	*first_b;
+	t_stack	*tmp;
 
 	if (stack_size(stack_a) == 0)
 		return ;
-	tmp_a = *stack_a;
-	while (tmp_a->next->next != *stack_a)
-		tmp_a = tmp_a->next;
-	first_a = tmp_a->next;
-	tmp_a->next = *stack_a;
+	tmp = *stack_a;
+	while (tmp->next->next != *stack_a)
+		tmp = tmp->next;
+	first_a = tmp->next;
+	tmp->next = *stack_a;
 	if (*stack_b)
 	{
-		tmp_b = *stack_b;
-		while (tmp_b->next->next != *stack_b)
-			tmp_b = tmp_b->next;
-		tmp_b->next = first_a;
+		first_b = *stack_b;
+		while (first_b->next->next != *stack_b)
+			first_b = first_b->next;
+		first_a->next = *stack_b;
+		first_b->next = first_a;
 	}
 	else
 	{
-		tmp_b = first_a;
-		*stack_b = tmp_b;
+		first_b = first_a;
+		*stack_b = first_b;
+		first_b->next = *stack_b;
 	}
-	tmp_b->next = *stack_b;
+	tmp = *stack_a;
+	if (tmp->value == first_b->value)
+		*stack_a = 0;
 }
