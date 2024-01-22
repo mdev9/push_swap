dd70be108891b0f8185dcc8b70ac723e51575b09
mdev9 <devriesmarijn05@gmail.com>
2024-01-22 10:50:45 +0100

    algorithm wip

--- push_swap.c
+++ push_swap.c
@@ -6,16 +6,54 @@
 /*   By: marde-vr <marde-vr@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 14:45:08 by marde-vr          #+#    #+#             */
-/*   Updated: 2024/01/21 22:30:01 by marde-vr         ###   ########.fr       */
+/*   Updated: 2024/01/22 10:48:26 by marde-vr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+t_stack	*top_node(t_stack *stack)
+{
+	if (!stack)
+		return (NULL);
+	while (stack->next != *stack)
+		stack = stack->next;
+	return (stack);
+}
+
 void	push_swap(t_stack *a, t_stack *b)
 {
-	(void)(a);
-	(void)(b);
+	t_stack	*node;
+	int lowest_cost;
+	int	curr_cost;
+
+	if (stack_size(a) < 6)
+		return ;
+	pb(a, b);
+	pb(a, b);
+	node = *a;
+	lowest_cost = calculate_cost(node, b);
+	while (stack_size(a) != 3 && node->next != *a)
+	{
+		node = node->next;
+		curr_cost = calculate_cost(node, b);
+		if (curr_cost < lowest_cost || curr_cost == 1)
+			push_node(node, a, b);
+	}
+
+	/*
+	while (*a != 0)
+	{
+		first_a = top_node(a);
+		first_b = top_node(b);
+		if (first_a->value > first_b->value || first_a < *b->value)
+		{
+			pb(a, b);
+			if (first_a->value < *b->value)
+				rb(b);
+
+		}
+	}*/
 	return ;
 }
 
