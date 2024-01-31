ae2cfba771e6b601a7d1ff08b01b906cdba6e81d
mdev9 <devriesmarijn05@gmail.com>
2024-01-31 11:14:52 +0100

    switched back to radix

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: marde-vr <marde-vr@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 14:45:08 by marde-vr          #+#    #+#             */
-/*   Updated: 2024/01/28 12:40:50 by marde-vr         ###   ########.fr       */
+/*   Updated: 2024/01/31 11:12:00 by marde-vr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,128 +73,6 @@ void	sort_stack_of_size_3(t_stack **stack_a)
 		rra(stack_a);
 }
 
-/*
-int	calc_rotations(t_stack *stack, int value)
-{
-	t_stack	*node;
-	int		i;
-
-	i = 0;
-	node = stack;
-	while (node->value != value)
-	{
-		node = node->next;
-		i++;
-	}
-	return (stack_size(&stack) - i);
-}
-
-int	total_cost(int a, int b)
-{
-	int	cost;
-
-	cost = 1;
-	if (a > b)
-		cost += a - b;
-	else if (a < b)
-		cost += a + b;
-	return (cost);
-}
-
-int	calculate_cost(int node_i, t_stack *stack_a, t_stack *stack_b)
-{
-	t_stack	*node;
-	int		min;
-	int		max;
-	int		i;
-
-	i = 0;
-	node = stack_a;
-	while (i < node_i)
-	{
-		node = node->next;
-		i++;
-	}
-	min = stack_min(stack_b)->value;
-	max = stack_max(stack_b)->value;
-	if (node->value > max || node->value < min)
-		return (total_cost(calc_rotations(stack_a, node->value),
-				calc_rotations(stack_b, max)));
-	return (1);
-}
-
-t_stack	*first(t_stack *stack)
-{
-	t_stack	*first;
-
-	first = stack;
-	while (first->next != stack)
-		first = first->next;
-	return (first);
-}
-
-void	empty_b(t_stack **a, t_stack **b)
-{
-	int	first_a;
-	int	first_b;
-
-	
-	first_a = first(*a)->value;
-	first_b = first(*b)->value;
-	while (stack_size(b))
-	{
-		while (first_a < first_b && first_b )
-		{
-			rra(a);
-			first_a = first(*a)->value;
-			first_b = first(*b)->value;
-			ft_printf("first_a: %d, first_b: %d\n", first_a, first_b);
-			print_stacks(*a, *b);
-		}
-		pa(a, b);
-		print_stacks(*a, *b);
-		//pa(a, b);
-	}
-}
-
-void	sort_stack(t_stack **a, t_stack **b)
-{
-	int	lowest_cost;
-	int	curr_cost;
-	int	i;
-
-	pb(a, b);
-	print_stacks(*a, *b);
-	pb(a, b);
-	print_stacks(*a, *b);
-	lowest_cost = calculate_cost(stack_size(a), *a, *b);
-	while (stack_size(a) > 3)
-	{
-		i = stack_size(a);
-		while (i > 0)
-		{
-			curr_cost = calculate_cost(i, *a, *b);
-			ft_printf("i: %d, cost: %d\n", i, curr_cost);
-			if (curr_cost < lowest_cost)
-				lowest_cost = curr_cost;
-			if (curr_cost == 1)
-				break ;
-			i--;
-		}
-		//push_to_b(node, a, b);
-	}
-	if (!stack_is_sorted(*a))
-	{
-		if (stack_size(a) == 2)
-			sa(a);
-		else
-			sort_stack_of_size_3(a);
-		print_stacks(*a, *b);
-	}
-	empty_b(a, b);
-}
-*/
-
 void	sort_stack(t_stack **a, t_stack **b)
 {
 	int	size;
@@ -232,6 +110,47 @@ void	sort_stack(t_stack **a, t_stack **b)
 	}
 }
 
+int	simplify_stack(t_stack **a)
+{
+	t_stack	*node;
+	int		*tmp;
+	int		size;
+	int		i;
+	int		j;
+
+	size = stack_size(a);
+	tmp = calloc(size, sizeof(int *));
+	if (!tmp)
+		return (1);
+	i = 0;
+	node = *a;
+	while (node->next != *a)
+	{
+		tmp[i] = node->value;
+		node = node->next;
+		i++;
+	}
+	i = 0;
+	node = *a;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size)
+		{
+			if (node->value == tmp[j])
+			{
+				node->value = j;
+				break ;
+			}
+			j++;
+		}
+		node = node->next;
+		i++;
+	}
+	free(tmp);
+	return (0);
+}
+
 void	push_swap(t_stack **a, t_stack **b)
 {
 	if (stack_is_sorted(*a))
@@ -247,7 +166,9 @@ void	push_swap(t_stack **a, t_stack **b)
 		return ;
 	}
 	else
+	{
 		sort_stack(a, b);
+	}
 	(void)b;
 }
 
@@ -263,6 +184,9 @@ int	main(int argc, char **argv)
 	ft_printf("a: ");
 	print_stack(stack_a);
 	ft_printf("stack sorted: %d\n", stack_is_sorted(stack_a));
+	if (simplify_stack(&stack_a))
+		return (1);
+	print_stack(stack_a);
 	push_swap(&stack_a, &stack_b);
 	print_stack(stack_a);
 	ft_printf("stack sorted: %d\n", stack_is_sorted(stack_a));
