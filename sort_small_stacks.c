c07f8cd43b28999c09df737008f2d93bd6258865
mdev9 <devriesmarijn05@gmail.com>
2024-02-01 20:10:55 +0100

    working on small stack sort function

--- sort_small_stacks.c
+++ sort_small_stacks.c
@@ -6,7 +6,7 @@
 /*   By: marde-vr <marde-vr@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 17:41:47 by marde-vr          #+#    #+#             */
-/*   Updated: 2024/02/01 19:33:57 by marde-vr         ###   ########.fr       */
+/*   Updated: 2024/02/01 20:09:44 by marde-vr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 t_stack	*stack_min(t_stack *stack) // remove ?
 {
-	t_stack	*node;
+	t_stack *node;
 
 	node = stack;
 	while (node->next->value < node->value)
@@ -24,7 +24,7 @@ t_stack	*stack_min(t_stack *stack) // remove ?
 
 t_stack	*stack_max(t_stack *stack) // remove ?
 {
-	t_stack	*node;
+	t_stack *node;
 
 	node = stack;
 	while (node->next->value > node->value)
@@ -61,24 +61,21 @@ void	sort_stack_of_size_3(t_stack **stack_a)
 
 void	sort_small_stack(t_stack **a, t_stack **b)
 {
+	//int	size;
+
+	//size = stack_size(a);
 	while (stack_size(a) != 3)
 		pb(a, b);
 	sort_stack_of_size_3(a);
 	print_stacks(*a, *b);
-	while ((top_node(*a)->value < top_node(*b)->value) || (top_node(*a) != stack_min(*a) && *a != stack_max(*a)))
-		ra(a);
+	if (stack_size(b) == 2 && !stack_is_sorted(*b))
+		sb(b);
 	print_stacks(*a, *b);
+	//while (top_node(*b) > top_node(*a))
+		
 	pa(a, b);
+	print_stacks(*a, *b);
 	if (*b)
-	{
-		while (!stack_is_sorted(*a))
-			ra(a);
-		while ((top_node(*a)->value < top_node(*b)->value) || (top_node(*a) != stack_min(*a) && *a != stack_max(*a)))
-			ra(a);
-		print_stacks(*a, *b);
 		pa(a, b);
-		print_stacks(*a, *b);
-	}
-	while (!stack_is_sorted(*a))
-		ra(a);
+	print_stacks(*a, *b);
 }
